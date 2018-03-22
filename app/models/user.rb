12f1c9dcee7733b2abf1f0f5179483f02cfe91c5
github username <github email address>
2018-03-22 11:22:26 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,6 @@ class User < ActiveRecord::Base
   has_many :listings, :foreign_key => 'guest_id'
   has_many :listings, :foreign_key => 'host_id'
   has_many :reviews, :foreign_key => 'guest_id'
-
-  has_many :trips do
-    self.reservations.map do |trip|
-      trip.guest_id == self.id
-    end
-  end
+  has_many :trips, class_name: 'Reservation', foreign_key: 'guest_id'
 
 end
